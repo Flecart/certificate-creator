fcc4a79a7866bdc33e473b367d9122fe28e6985d
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-07-05 14:21:37 +0200

    feat: create endpoint update type

---
+++ lists.ts
@@ -0,0 +1,13 @@
+export interface CertListItem {
+    name: string;
+    link: string; // url
+}
+
+export const listBucketName = "lists";
+
+export const certificateLists: CertListItem[] = [
+    {
+        "name": "lead-the-future",
+        "link": "https://api.gomry.co/v1/contacts?listID=YB8Ef1QBF50qrAz16h5W"
+    }
+]
