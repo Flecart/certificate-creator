fe1c75481f815900d682dc0a2e7f25d7583e87cc
Francesco Capponi <1690009+FreCap@users.noreply.github.com>
2023-10-03 12:01:38 +0200

    Adding signature mode

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ It works by storing the generated certificates in a supabase bucket.
 ```
 npm run dev
 ```
+
+
+Open address
+```
+http://localhost:3000/api/certificate/createAdmin?fullName=Nome%20Cognome&keySuperUser=PUTYOURKEY
+
+http://localhost:3000/api/certificate/createAdmin?fullName=Nome%20Cognome&paramSignedSuperUser=986f862cdasjndj465c98fbd25eb0480b01d7b81a71d3c199eec8623ff501b8
+```
--- pages/api/certificate/createAdmin.ts
+++ pages/api/certificate/createAdmin.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import getConfig from 'next/config'
+import crypto from 'crypto';
 
 import { createHash } from 'crypto';
 import {createPDF} from '@/pages/api/certificate/create'
@@ -16,15 +17,20 @@ export default async function handler(
 
     const fullName = req.query.fullName as string || "";
     const keySuperUser = req.query.keySuperUser as string || "";
+    const signerName = req.query.paramSignedSuperUser as string || "";
 
     let name : string;
 
     if (!fullName) {
         return res.status(400).json({ error: 'Missing name' })
     }
-    
-    if (keySuperUser != serverRuntimeConfig.APICreateSuperUserKey) {
-        return res.status(401).json({ error: 'Wrong key '+serverRuntimeConfig.APICreateSuperUserKey+" keySuperUser "+keySuperUser })
+
+    if(signerName != ""){
+        if(!verifySignature(fullName, serverRuntimeConfig.APICreateSuperUserKey, signerName )){
+            return res.status(401).json({ error: 'Invalid signature for name ' + fullName})
+        }
+    } else if (keySuperUser != serverRuntimeConfig.APICreateSuperUserKey) {
+        return res.status(401).json({ error: 'Wrong key keySuperUser ' + keySuperUser })
     }
 
     name = fullName;
@@ -51,4 +57,12 @@ export default async function handler(
 
 function calculateMD5(input: string): string {
     return createHash('md5').update(input).digest('hex');
-  }
+}
+
+function verifySignature(message: string, secret: string, signature: string): boolean {
+  const hash = crypto.createHmac('sha256', secret)
+    .update(message)
+    .digest('hex');
+
+  return hash === signature;
+}
